ec6b0cea6ea09e0fe1cc7fa1c2d1d2c3f6d42494
0xdevabir <abir573912@gmail.com>
2025-01-25 00:14:17 +0600

    dn

--- src/app/sections/head.tsx
+++ src/app/sections/head.tsx
@@ -10,6 +10,7 @@ export default function Head() {
               <div className="absolute left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%]">
                   <p className="text-white text-[20vw] sm:text-[10vw] leading-[0px] sm:leading-[100px] font-[800] text-center cursor-pointer"><span className="text-[#00d5ff]">web</span><span className="text-[#96eefb]">ne</span>st</p>
                   <p className="text-center text-white text-[2.2vw] leading-[70px] sm:text-[1.4vw] font-[500] sm:font-[300] tracking-[2.2px] sm:tracking-[5px] cursor-pointer">"Elevate Your Online Presence with WebNest"</p>
+                  <p>hello this is abir</p>
               </div>
               <div className="absolute left-[52.3%] top-[55%] translate-x-[-50%] translate-y-[-50%] z-0 cursor-pointer">
                   {/* <img className="astro-img-1" src="images/astro.png" alt=""> */}
