de4e913166ce78d9d2d5c6f3410cf7b9d3ad6ac9
0xdevabir <abir573912@gmail.com>
2025-01-25 22:58:16 +0600

    mdn

--- src/app/sections/head.tsx
+++ src/app/sections/head.tsx
@@ -9,7 +9,7 @@ export default function Head() {
           <div className='h-[80vh] leading-[100px] relative'>
               <div className='absolute left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%]'>
                   <p className=' text-white text-[20vw] sm:text-[10vw] leading-[0px] sm:leading-[100px] font-[800] text-center cursor-pointer'><span className='text-[#00d5ff]'>web</span><span className='text-[#96eefb]'>ne</span>st</p>
-                  <p className='text-center text-white text-[2.2vw] leading-[70px] sm:text-[1.4vw] font-[500] sm:font-[300] tracking-[2.2px] sm:tracking-[5px] cursor-pointer'>{'Elevate Your Online Presence with WebNest'}</p>
+                  <p className='text-center text-white text-[2.2vw] leading-[70px] sm:text-[1.4vw] font-[500] sm:font-[300] tracking-[2.2px] sm:tracking-[5px] cursor-pointer'>&apos;Elevate Your Online Presence with WebNest&apos;</p>
              
 
               </div>
