902f4f2f4d3513b2e706e97ba3263227f70bac9d
0xdevabir <abir573912@gmail.com>
2025-02-02 16:03:14 +0600

    dn

--- src/component/about.tsx
+++ src/component/about.tsx
@@ -1,7 +1,113 @@
+import Image from "next/image";
+
 export default function About() {
   return (
-    <div className="">
-        <p className="h-[100vh]">About</p>
+    <div className="h-[200vh]">
+        <div className="bg-white h-[50vh] flex justify-center items-center">
+            <p className="text-[100px] font-[700] text-[#000000e0]">Our Projects</p>
+        </div>
+        <div className="flex justify-center">
+            <div className="bg-[black] rounded-tr-[100px] w-[25vw] h-[30vh] flex justify-center items-center">
+                <div className="flex justify-center items-center">
+
+                  <Image src="/hot-tech.png" alt="Hot Tech" width={500} height={300} className="w-[85%] h-[85%] rounded-[60px]" />
+
+                </div>
+            </div>
+            <div className="bg-black"><div className="bg-[white] rounded-bl-[100px] w-[25vw] h-[30vh] flex justify-center items-center">
+                <div className="flex justify-center items-center">
+
+                  <Image src="/hot-tech.png" alt="Hot Tech" width={500} height={300} className="w-[85%] h-[85%] rounded-[60px]" />
+
+                </div>
+              </div></div>
+            <div className="bg-[white] w-[25vw] h-[30vh] flex justify-center items-center">
+              <div className="flex justify-center items-center">
+
+                <Image src="/hot-tech.png" alt="Hot Tech" width={500} height={300} className="w-[85%] h-[85%] rounded-[60px]" />
+
+              </div>
+            </div>
+            <div className="bg-[white] w-[25vw] h-[30vh] flex justify-center items-center">
+                <div className="flex justify-center items-center">
+
+                <Image src="/hot-tech.png" alt="Hot Tech" width={500} height={300} className="w-[85%] h-[85%] rounded-[60px]" />
+
+                </div>
+            </div>
+
+        </div>
+        <div className="flex justify-center">
+            <div className="bg-[black] w-[25vw] h-[30vh] flex justify-center items-center">
+              <div className="flex justify-center items-center">
+
+                <Image src="/hot-tech.png" alt="Hot Tech" width={500} height={300} className="w-[85%] h-[85%] rounded-[60px]" />
+
+              </div>
+            </div>
+            <div className="bg-[black] rounded-tr-[100px] w-[25vw] h-[30vh] flex justify-center items-center">
+                <div className="flex justify-center items-center">
+
+                  <Image src="/hot-tech.png" alt="Hot Tech" width={500} height={300} className="w-[85%] h-[85%] rounded-[60px]" />
+
+                </div>
+            </div>
+            <div className="bg-black"><div className="bg-[white] rounded-bl-[100px] w-[25vw] h-[30vh] flex justify-center items-center">              <div className="flex justify-center items-center">
+
+<Image src="/hot-tech.png" alt="Hot Tech" width={500} height={300} className="w-[85%] h-[85%] rounded-[60px]" />
+
+</div></div></div>
+            <div className="bg-[white] w-[25vw] h-[30vh] flex justify-center items-center">              <div className="flex justify-center items-center">
+
+<Image src="/hot-tech.png" alt="Hot Tech" width={500} height={300} className="w-[85%] h-[85%] rounded-[60px]" />
+
+</div></div>
+        </div>
+        <div className="flex justify-center">
+            <div className="bg-[black] w-[25vw] h-[30vh] flex justify-center items-center">              <div className="flex justify-center items-center">
+
+<Image src="/hot-tech.png" alt="Hot Tech" width={500} height={300} className="w-[85%] h-[85%] rounded-[60px]" />
+
+</div></div>
+            <div className="bg-[black] w-[25vw] h-[30vh] flex justify-center items-center">              <div className="flex justify-center items-center">
+
+<Image src="/hot-tech.png" alt="Hot Tech" width={500} height={300} className="w-[85%] h-[85%] rounded-[60px]" />
+
+</div></div>
+            <div className="bg-[black] rounded-tr-[100px] w-[25vw] h-[30vh] flex justify-center items-center"><div className="flex justify-center items-center">
+
+<Image src="/hot-tech.png" alt="Hot Tech" width={500} height={300} className="w-[85%] h-[85%] rounded-[60px]" />
+
+</div></div>
+            <div className="bg-black"><div className="bg-[white] rounded-bl-[100px] w-[25vw] h-[30vh] flex justify-center items-center"><div className="flex justify-center items-center">
+
+<Image src="/hot-tech.png" alt="Hot Tech" width={500} height={300} className="w-[85%] h-[85%] rounded-[60px]" />
+
+</div></div></div>
+
+        </div>
+        <div className="flex justify-center">
+            <div className="bg-[black] w-[25vw] h-[30vh] flex justify-center items-center"><div className="flex justify-center items-center">
+
+<Image src="/hot-tech.png" alt="Hot Tech" width={500} height={300} className="w-[85%] h-[85%] rounded-[60px]" />
+
+</div></div>
+            <div className="bg-[black] w-[25vw] h-[30vh] flex justify-center items-center"><div className="flex justify-center items-center">
+
+<Image src="/hot-tech.png" alt="Hot Tech" width={500} height={300} className="w-[85%] h-[85%] rounded-[60px]" />
+
+</div></div>
+            <div className="bg-[black] w-[25vw] h-[30vh] flex justify-center items-center"><div className="flex justify-center items-center">
+
+<Image src="/hot-tech.png" alt="Hot Tech" width={500} height={300} className="w-[85%] h-[85%] rounded-[60px]" />
+
+</div></div>
+            <div className="bg-[black] w-[25vw] h-[30vh] rounded-tr-[100px] flex justify-center items-center"><div className="flex justify-center items-center">
+
+<Image src="/hot-tech.png" alt="Hot Tech" width={500} height={300} className="w-[85%] h-[85%] rounded-[60px]" />
+
+</div></div>
+        </div>
     </div>
   );
 }
