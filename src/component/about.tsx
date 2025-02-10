5e50b630cfd12b3341e76a3b73061f79ddc87717
0xdevabir <abir573912@gmail.com>
2025-02-10 20:45:45 +0600

    dn

--- src/component/about.tsx
+++ src/component/about.tsx
@@ -1,6 +1,6 @@
 "use client";
 import Image from "next/image";
-import { motion } from "framer-motion";
+import Link from "next/link";
 export default function About() {
   return (
 //     <div className="h-[200vh] hidden">
@@ -121,17 +121,13 @@ export default function About() {
 
 
 
-<div className="h-[200vh] w-[100vw]">
-        <div className="bg-white h-[50vh] flex justify-center items-center">
-            <p className="text-[100px] font-[700] text-[#000000e0]">Our Projects</p>
+<div className=" w-[100vw]">
+        <div className="bg-white h-[30vh] md:h-[50vh] flex justify-center items-center">
+            <p className="text-[30px] md:text-[100px] font-[700] text-[#000000e0]">Our Projects</p>
         </div>
         <div className="flex justify-center flex-wrap">
-            <div className="bg-[red] h-[300px] w-[100vw] md:w-[50vw] 2xl:w-[25vw] flex justify-center items-center">
-                  {/* <div className="flex justify-center items-center">
-
-                    <Image src="/hot-tech.png" alt="Hot Tech" width={500} height={300} className="w-[85%] h-[85%] rounded-[20px]" />
-
-                  </div> */}
+            <div className=" h-[300px] w-[100vw] md:w-[50vw] 2xl:w-[25vw] flex justify-center items-center">
+                  
                   <div className="relative w-[85%] h-[85%] group cursor-pointer">
                       {/* Image */}
                       <Image
@@ -142,14 +138,17 @@ export default function About() {
                       />
 
                       {/* Hover Overlay */}
-                      <div className="absolute inset-0 bg-black bg-opacity-70 flex items-center justify-center opacity-0 group-hover:opacity-100  transition-opacity duration-300 rounded-lg">
+                      <div className="absolute inset-0 bg-black bg-opacity-85 flex items-center justify-center opacity-0 group-hover:opacity-100  transition-opacity duration-300 rounded-lg">
                         <div className="flex w-[100%]">
                           <div className="w-[80%]">
-                            <p className="text-white font-[600]">More info</p>
+                            <div className="">
+                              <p className="text-white font-[600] text-center text-[22px]">This is an E-Commerce Web</p>
+                              <p className="text-white font-[600] text-center text-[22px]">Tech: HTML, CSS, Bootstrap</p>
+                            </div>
                           </div>
                           <div className="w-[20%]">
 
-                            <Image src="/arrow.png" alt="Hot Tech" width={50} height={50} className="w-[85%] h-[85%] rotate-[180deg]" />
+                            <Link href="/"> <Image src="/arrow.png" alt="Hot Tech" width={50} height={50} className="w-[85%] h-[85%] rotate-[180deg]" /></Link>
 
                           </div>
                         </div>
@@ -157,11 +156,219 @@ export default function About() {
                 </div>
                    
             </div>
-            <div className="bg-[purple] h-[300px] w-[100vw] md:w-[50vw] 2xl:w-[25vw]">hello</div>
-            <div className="bg-[red] h-[300px] w-[100vw] md:w-[50vw] 2xl:w-[25vw]">hello</div>
-            <div className="bg-[green] h-[300px] w-[100vw] md:w-[50vw] 2xl:w-[25vw]">hello</div>
+            <div className=" h-[300px] w-[100vw] md:w-[50vw] 2xl:w-[25vw] flex justify-center items-center">
+                  
+                  <div className="relative w-[85%] h-[85%] group cursor-pointer">
+                      {/* Image */}
+                      <Image
+                        src="/hot-tech.png"
+                        alt="Hot Tech"
+                        layout="fill"
+                        className="object-cover rounded-lg"
+                      />
+
+                      {/* Hover Overlay */}
+                      <div className="absolute inset-0 bg-black bg-opacity-85 flex items-center justify-center opacity-0 group-hover:opacity-100  transition-opacity duration-300 rounded-lg">
+                        <div className="flex w-[100%]">
+                          <div className="w-[80%]">
+                            <div className="">
+                              <p className="text-white font-[600] text-center text-[22px]">This is an E-Commerce Web</p>
+                              <p className="text-white font-[600] text-center text-[22px]">Tech: HTML, CSS, Bootstrap</p>
+                            </div>
+                          </div>
+                          <div className="w-[20%]">
+
+                            <Link href="/"> <Image src="/arrow.png" alt="Hot Tech" width={50} height={50} className="w-[85%] h-[85%] rotate-[180deg]" /></Link>
+
+                          </div>
+                        </div>
+                      </div>
+                </div>
+                   
+            </div> <div className=" h-[300px] w-[100vw] md:w-[50vw] 2xl:w-[25vw] flex justify-center items-center">
+                  
+                  <div className="relative w-[85%] h-[85%] group cursor-pointer">
+                      {/* Image */}
+                      <Image
+                        src="/hot-tech.png"
+                        alt="Hot Tech"
+                        layout="fill"
+                        className="object-cover rounded-lg"
+                      />
+
+                      {/* Hover Overlay */}
+                      <div className="absolute inset-0 bg-black bg-opacity-85 flex items-center justify-center opacity-0 group-hover:opacity-100  transition-opacity duration-300 rounded-lg">
+                        <div className="flex w-[100%]">
+                          <div className="w-[80%]">
+                            <div className="">
+                              <p className="text-white font-[600] text-center text-[22px]">This is an E-Commerce Web</p>
+                              <p className="text-white font-[600] text-center text-[22px]">Tech: HTML, CSS, Bootstrap</p>
+                            </div>
+                          </div>
+                          <div className="w-[20%]">
+
+                            <Link href="/"> <Image src="/arrow.png" alt="Hot Tech" width={50} height={50} className="w-[85%] h-[85%] rotate-[180deg]" /></Link>
+
+                          </div>
+                        </div>
+                      </div>
+                </div>
+                   
+            </div> <div className=" h-[300px] w-[100vw] md:w-[50vw] 2xl:w-[25vw] flex justify-center items-center">
+                  
+                  <div className="relative w-[85%] h-[85%] group cursor-pointer">
+                      {/* Image */}
+                      <Image
+                        src="/hot-tech.png"
+                        alt="Hot Tech"
+                        layout="fill"
+                        className="object-cover rounded-lg"
+                      />
+
+                      {/* Hover Overlay */}
+                      <div className="absolute inset-0 bg-black bg-opacity-85 flex items-center justify-center opacity-0 group-hover:opacity-100  transition-opacity duration-300 rounded-lg">
+                        <div className="flex w-[100%]">
+                          <div className="w-[80%]">
+                            <div className="">
+                              <p className="text-white font-[600] text-center text-[22px]">This is an E-Commerce Web</p>
+                              <p className="text-white font-[600] text-center text-[22px]">Tech: HTML, CSS, Bootstrap</p>
+                            </div>
+                          </div>
+                          <div className="w-[20%]">
+
+                            <Link href="/"> <Image src="/arrow.png" alt="Hot Tech" width={50} height={50} className="w-[85%] h-[85%] rotate-[180deg]" /></Link>
+
+                          </div>
+                        </div>
+                      </div>
+                </div>
+                   
+            </div>
         </div>
+        {/* .................. */}
+        <div className="flex justify-center flex-wrap">
+            <div className="h-[300px] w-[100vw] md:w-[50vw] 2xl:w-[25vw] flex justify-center items-center">
+                  
+                  <div className="relative w-[85%] h-[85%] group cursor-pointer">
+                      {/* Image */}
+                      <Image
+                        src="/hot-tech.png"
+                        alt="Hot Tech"
+                        layout="fill"
+                        className="object-cover rounded-lg"
+                      />
+
+                      {/* Hover Overlay */}
+                      <div className="absolute inset-0 bg-black bg-opacity-85 flex items-center justify-center opacity-0 group-hover:opacity-100  transition-opacity duration-300 rounded-lg">
+                        <div className="flex w-[100%]">
+                          <div className="w-[80%]">
+                            <div className="">
+                              <p className="text-white font-[600] text-center text-[22px]">This is an E-Commerce Web</p>
+                              <p className="text-white font-[600] text-center text-[22px]">Tech: HTML, CSS, Bootstrap</p>
+                            </div>
+                          </div>
+                          <div className="w-[20%]">
+
+                            <Link href="/"> <Image src="/arrow.png" alt="Hot Tech" width={50} height={50} className="w-[85%] h-[85%] rotate-[180deg]" /></Link>
+
+                          </div>
+                        </div>
+                      </div>
+                </div>
+                   
+            </div>
+            <div className=" h-[300px] w-[100vw] md:w-[50vw] 2xl:w-[25vw] flex justify-center items-center">
+                  
+                  <div className="relative w-[85%] h-[85%] group cursor-pointer">
+                      {/* Image */}
+                      <Image
+                        src="/hot-tech.png"
+                        alt="Hot Tech"
+                        layout="fill"
+                        className="object-cover rounded-lg"
+                      />
+
+                      {/* Hover Overlay */}
+                      <div className="absolute inset-0 bg-black bg-opacity-85 flex items-center justify-center opacity-0 group-hover:opacity-100  transition-opacity duration-300 rounded-lg">
+                        <div className="flex w-[100%]">
+                          <div className="w-[80%]">
+                            <div className="">
+                              <p className="text-white font-[600] text-center text-[22px]">This is an E-Commerce Web</p>
+                              <p className="text-white font-[600] text-center text-[22px]">Tech: HTML, CSS, Bootstrap</p>
+                            </div>
+                          </div>
+                          <div className="w-[20%]">
+
+                            <Link href="/"> <Image src="/arrow.png" alt="Hot Tech" width={50} height={50} className="w-[85%] h-[85%] rotate-[180deg]" /></Link>
+
+                          </div>
+                        </div>
+                      </div>
+                </div>
+                   
+            </div> <div className=" h-[300px] w-[100vw] md:w-[50vw] 2xl:w-[25vw] flex justify-center items-center">
+                  
+                  <div className="relative w-[85%] h-[85%] group cursor-pointer">
+                      {/* Image */}
+                      <Image
+                        src="/hot-tech.png"
+                        alt="Hot Tech"
+                        layout="fill"
+                        className="object-cover rounded-lg"
+                      />
+
+                      {/* Hover Overlay */}
+                      <div className="absolute inset-0 bg-black bg-opacity-85 flex items-center justify-center opacity-0 group-hover:opacity-100  transition-opacity duration-300 rounded-lg">
+                        <div className="flex w-[100%]">
+                          <div className="w-[80%]">
+                            <div className="">
+                              <p className="text-white font-[600] text-center text-[22px]">This is an E-Commerce Web</p>
+                              <p className="text-white font-[600] text-center text-[22px]">Tech: HTML, CSS, Bootstrap</p>
+                            </div>
+                          </div>
+                          <div className="w-[20%]">
+
+                            <Link href="/"> <Image src="/arrow.png" alt="Hot Tech" width={50} height={50} className="w-[85%] h-[85%] rotate-[180deg]" /></Link>
+
+                          </div>
+                        </div>
+                      </div>
+                </div>
+                   
+            </div> <div className=" h-[300px] w-[100vw] md:w-[50vw] 2xl:w-[25vw] flex justify-center items-center">
+                  
+                  <div className="relative w-[85%] h-[85%] group cursor-pointer">
+                      {/* Image */}
+                      <Image
+                        src="/hot-tech.png"
+                        alt="Hot Tech"
+                        layout="fill"
+                        className="object-cover rounded-lg"
+                      />
+
+                      {/* Hover Overlay */}
+                      <div className="absolute inset-0 bg-black bg-opacity-85 flex items-center justify-center opacity-0 group-hover:opacity-100  transition-opacity duration-300 rounded-lg">
+                        <div className="flex w-[100%]">
+                          <div className="w-[80%]">
+                            <div className="">
+                              <p className="text-white font-[600] text-center text-[22px]">This is an E-Commerce Web</p>
+                              <p className="text-white font-[600] text-center text-[22px]">Tech: HTML, CSS, Bootstrap</p>
+                            </div>
+                          </div>
+                          <div className="w-[20%]">
+
+                            <Link href="/"> <Image src="/arrow.png" alt="Hot Tech" width={50} height={50} className="w-[85%] h-[85%] rotate-[180deg]" /></Link>
+
+                          </div>
+                        </div>
+                      </div>
+                </div>
+                   
+            </div>
+        </div>
+
 </div>
+
 
 
   );
