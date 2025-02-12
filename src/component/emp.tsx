27a92fe7653f4548632ad3a616aab2e7efee2e43
0xdevabir <abir573912@gmail.com>
2025-02-12 18:46:11 +0600

    tmd

--- src/component/emp.tsx
+++ src/component/emp.tsx
@@ -43,7 +43,7 @@ export default function Team() {
             <p className="text-[30px] md:text-[100px] font-[700] text-[#000000e0]" data-aos="fade-up">Our Team</p>
         </div>
 
-        <div className="w-[100vw] flex flex-wrap justify-around">
+        <div className="w-[100vw] flex flex-wrap justify-evenly">
 
          
             <div className=" bg-gradient-to-br from-[#0a192f] to-[#121212] py-0 md:py-[50px] w-[90%] md:w-[40%] rounded-[28px]" data-aos="fade-right">
@@ -119,10 +119,10 @@ export default function Team() {
 
 
 
-        <div className="w-[100vw] flex flex-wrap justify-around mt-[20vh]">
+        <div className="w-[100vw] flex flex-wrap justify-evenly mt-[14vh]">
 
                 
-        <div className="bg-[#000000] py-0 md:py-[50px] w-[90%] md:w-[35%] rounded-[28px]" data-aos="fade-right">
+        <div className="bg-gradient-to-br from-[#0a192f] to-[#121212] py-0 md:py-[50px] w-[90%] md:w-[40%] rounded-[28px]" data-aos="fade-right">
             <div className="flex flex-wrap items-center w-[100%]">
                 <div className="w-[100%] md:w-[40%] flex justify-center" data-aos="fade-right">
                   <Image
@@ -130,11 +130,11 @@ export default function Team() {
                         alt="Hot Tech"
                         width={5000}
                         height={5000}
-                        className="w-[200px] md:w-[350px] relative right-0 md:right-[50px] bottom-[80px] md:bottom-0 rounded-[350px] md:rounded-[28px] border-[6px] md:border-[0px] border-black"
+                        className="w-[200px] md:w-[350px] relative right-0 md:right-[50px] bottom-[80px] md:bottom-0 rounded-[350px] md:rounded-[28px] border-[6px] md:border-[0px] border-black hover:scale-[1.02] transition-all cursor-pointer"
                     />
                 </div>
                 <div className="w-[100%] md:w-[60%] pl-[25px] md:pl-0 pr-[25px] relative bottom-[40px] md:bottom-0 text-center md:text-left">
-                    <p className="text-[28px] font-[700] text-white">KHALID HOSSAIN ARAFAT</p>
+                    <p className="text-[28px] font-[700] text-white tracking-[3px]">KHALID HOSSAIN ARAFAT</p>
                     <p className="text-[18px] font-[600] text-[#fffffff2] py-[20px]">Co-Founder & COO</p>
                     <p className="text-[16px] font-[400] text-[#ffffffde] text-center md:text-left">Keeping the team aligned, projects on track, and operations running smoothly. Loves building efficient systems.</p>
                     <div className="py-[20px] flex justify-center md:justify-start gap-5">
@@ -146,7 +146,7 @@ export default function Team() {
                 </div>
             </div>
         </div>
-        <div className="bg-[#000000] py-0 md:py-[50px] w-[90%] md:w-[35%] rounded-[28px] mt-[20vh] md:mt-0" data-aos="fade-left">
+        <div className="bg-gradient-to-tr from-[#121212] to-[#0a192f] py-0 md:py-[50px] w-[90%] md:w-[40%] rounded-[28px] mt-[20vh] md:mt-0" data-aos="fade-left">
             <div className="flex flex-wrap items-center w-[100%]">
 
             <div className="w-[100%] md:w-[40%] flex justify-center md:hidden" data-aos="fade-left">
@@ -160,7 +160,7 @@ export default function Team() {
                 </div>
                 
                 <div className="w-[100%] md:w-[60%] pr-[25px] md:pr-0 pl-[25px] relative bottom-[40px] md:bottom-0 text-center md:text-right">
-                    <p className="text-[28px] font-[700] text-white">TOUHIDUL ISLAM</p>
+                    <p className="text-[28px] font-[700] text-white tracking-[3px]">TOUHIDUL ISLAM</p>
                     <p className="text-[18px] font-[600] text-[#fffffff2] py-[20px]">UX/UI Designer</p>
                     <p className="text-[16px] font-[400] text-[#ffffffde] text-center md:text-right">Crafting beautiful, intuitive designs that prioritize user experience. Passionate about blending creativity with functionality.</p>
                     <div className="py-[20px] flex justify-center md:justify-end gap-5">
@@ -170,13 +170,13 @@ export default function Team() {
                     </div>
 
                 </div>
-                <div className="w-[100%] md:w-[40%] flex justify-center hidden md:block">
+                <div className="w-[100%] md:w-[40%] flex justify-center hidden md:block" data-aos="fade-left">
                   <Image
                         src="/touhid.jpeg"
                         alt="Hot Tech"
                         width={5000}
                         height={5000}
-                        className="w-[200px] md:w-[350px] relative left-0 md:left-[50px] bottom-[80px] md:bottom-0 rounded-[350px] md:rounded-[28px] border-[6px] md:border-[0px] border-black"
+                        className="w-[200px] md:w-[350px] relative left-0 md:left-[50px] bottom-[80px] md:bottom-0 rounded-[350px] md:rounded-[28px] border-[6px] md:border-[0px] border-black hover:scale-[1.02] transition-all cursor-pointer"
                     />
                 </div>
 
