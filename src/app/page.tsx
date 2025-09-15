0ca494825381064dfbb4527e6fb06fe8a15ac46b
0xdevabir <devabir.me@gmail.com>
2025-09-15 19:14:16 +0600

    nfix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,11 @@ const Home = () => {
   return (
     <div>
         <div className="flex justify-center items-center flex-col min-h-screen bg-[wheat]">
+          <p className='text-[180px] font-[900] mb-4'>
+            <span className="text-[#00d5ff]">web</span>
+            <span className="text-[#96eefb]">ne</span>
+            <span className="text-[#F5F5F5]">st</span>
+          </p>
           <Image
               src="/maintenance.png"
               alt="maintenance"
