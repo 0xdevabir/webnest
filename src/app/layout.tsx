48b176b3e4517cb54c687e55eceb0323673023f4
0xdevabir <devabir.me@gmail.com>
2025-10-25 12:00:13 +0600

    ad

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,6 +30,8 @@ export default function RootLayout({
 
   return (
     <html lang="en">
+      <head><script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5819404985426974"
+     crossOrigin="anonymous"></script></head>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased `}>
         <Navbar/>
         {children}
