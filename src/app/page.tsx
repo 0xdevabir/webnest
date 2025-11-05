b21c8ec1ded68439ec9a6c683936657d5edab759
0xdevabir <devabir.me@gmail.com>
2025-11-05 16:38:25 +0600

    Home hero redesign

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,46 +5,26 @@ const Home = () => {
   return (
     <div>
         <div className="flex justify-center items-center flex-col min-h-screen bg-[#F5DEB3]">
-          <p className='text-[60px] md:text-[180px] font-[900]'>
-            <span className="text-[#2F3737]">web</span>
-            <span className="text-[#A8BCA1]">nest</span>
-          </p>
-          {/* 1F1F1F 3F4E4F */}
-          {/* <p className='text-[180px] font-[900]'>
-            <span className="text-[#1E293B]">web</span>
-            <span className="text-[#9CA3AF]">nest</span>
-          </p>
-          <p className='text-[180px] font-[900]'>
-            <span className="text-[#2A2A2A]">web</span>
-            <span className="text-[#B8996F]">nest</span>
-          </p>
-          <p className='text-[180px] font-[900]'>
-            <span className="text-[#A78BFA]">web</span>
-            <span className="text-[#9CA3AF]">ne</span>
-            <span className="text-[#9CA3AF]">st</span>
-          </p>
-          <p className='text-[180px] font-[900]'>
-            <span className="text-[#2B2D42]">web</span>
-            <span className="text-[#8D99AE]">ne</span>
-            <span className="text-[#D4A373]">st</span>
-          </p>
-          <p className='text-[180px] font-[900]'>
-            <span className="text-[#2A2A2A]">web</span>
-            <span className="text-[#C5A880]">ne</span>
-            <span className="text-[#C5A880]">st</span>
-          </p>
-          <p className='text-[180px] font-[900] mb-4'>
-            <span className="text-[#1E293B]">web</span>
-            <span className="text-[#94A3B8]">ne</span>
-            <span className="text-[#E2E8F0]">st</span>
-          </p> */}
-          <Image
+          <div className="leading-none">
+              <p className='text-[70px] sm:text-[120px] md:text-[160px] lg:text-[180px] font-[900]'>
+                <span className="text-[#2F3737]">web</span>
+                <span className="text-[#A8BCA1]">nest</span>
+              </p>
+              <div className="flex justify-around">
+                <p className='text-center leading-none text-[14px] sm:text-[22px] md:text-[34px] lg:text-[44px] font-[800] text-[#A8BCA1]'>Code.</p>
+                <p className='text-center leading-none text-[14px] sm:text-[22px] md:text-[34px] lg:text-[44px] font-[800] text-[#A8BCA1]'>Design.</p>
+                <p className='text-center leading-none text-[14px] sm:text-[22px] md:text-[34px] lg:text-[44px] font-[800] text-[#2F3737]'>Launch.</p>
+                <p className='text-center leading-none text-[14px] sm:text-[22px] md:text-[34px] lg:text-[44px] font-[800] text-[#2F3737]'>Repeat.</p>
+              </div>
+          </div>
+
+          {/* <Image
               src="/maintenance.png"
               alt="maintenance"
               width={200}
               height={200}
           />
-          <h1 className="text-[32px] font-[600]">In Maintenance</h1>
+          <h1 className="text-[32px] font-[600]">In Maintenance</h1> */}
         </div>
     </div>
   )
